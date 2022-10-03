0a35369e9863a9c915524aa10486afd8be529665
GStechschulte <stechschulteg@gmail.com>
2022-10-03 10:41:05 +0200

    random walk metropolis-hastings

--- Probability/samplers_metroplis_hasting_v2.py
+++ Probability/samplers_metroplis_hasting_v2.py
@@ -1,17 +1,23 @@
 import torch
 from torch import distributions as dist
 import matplotlib.pyplot as plt
-
+import argparse
+import warnings
+warnings.filterwarnings("ignore")
 
 def plot(distribution, trace_history, xmin, xmax, n_iterations, n_evals=500):
 
     x_evals = torch.linspace(xmin, xmax, n_evals)
     evals = torch.exp(distribution.log_prob(x_evals))
     
-    fig, ax = plt.subplots(1, 2, figsize=(12, 5))
+    fig = plt.figure(figsize=(12, 5))
+    ax = plt.axes(projection='3d')
 
-    ax[0].plot(x_evals, evals)
-    ax[1].plot(torch.arange(n_iterations), trace_history)
+    ax.plot(torch.arange(n_iterations), trace_history)
+    ax.plot(torch.zeros(n_evals), x_evals, evals)
+    
+    ax.set_xlabel('Iterations')
+    ax.set_ylabel('Samples')
     
     plt.show()
 
@@ -30,35 +36,48 @@ def true_distribution(mixture_probs, mus, scales):
         )
 
 def metropolis_hasting(x0, tau, mixture, n_iterations, rng_key=None):
+    """
+    implements the random walk metropolis-hasting algorithm
+    """
 
     x_current = x0
 
     x_samples = torch.zeros(n_iterations)
     x_samples[0] = x_current
+    cnt_acceptance = 0
      
     for n in range(1, n_iterations):
-
-        x_candidate = x_current + tau + dist.Normal(loc=0, scale=1).sample()
-        print(x_candidate)
+        
+        print(f'iteration       = {n}')
+        x_candidate = x_current + tau * dist.Normal(loc=0, scale=1).sample()
+        print(f'x_candidate     = {x_candidate}')
         p_candidate = torch.exp(mixture.log_prob(x_candidate))
-        print(p_candidate)
+        print(f'p_candidate     = {p_candidate}')
         p_current = torch.exp(mixture.log_prob(x_current))
-        print(p_current)
+        print(f'p_current       = {p_current}')
         alpha = p_candidate / p_current
-        print(alpha)
+        print(f'alpha           = {alpha}')
         probs_accept = min(1, alpha)
-        print(probs_accept)
-        #u = torch.rand(1)
+        print(f'probs_accept    = {probs_accept}')
         u = dist.Uniform(0, 1).sample()
-        print(u)
-        x_current = x_current if u >= probs_accept else x_candidate
+        print(f'uniform sample  = {u}', '\n')
+
+        if u >= probs_accept:
+            x_current = x_current
+        else:
+            x_current = x_candidate
+            cnt_acceptance += 1
+
         x_samples[n] = x_current
 
+    acceptence_probs = cnt_acceptance / n_iterations
+    print('--- statistics ---')
+    print(f'acceptance rate = {acceptence_probs}')
 
     return x_samples
 
 
-def main():
+def main(args):
 
     x0 = torch.tensor(20.) ## initial parameter value
 
@@ -66,15 +85,21 @@ def main():
     mus = torch.tensor([-20., 20.])
     scales = torch.tensor([10., 10.])
 
-    n_iters = 1000
-    tau = torch.tensor(8.)
+    n_iters = args.iters
+    tau = torch.tensor(args.tau)
 
     mixture_distribution = true_distribution(mixture_probs, mus, scales)
     x_samples = metropolis_hasting(x0, tau, mixture_distribution, n_iters)
 
-    plot(mixture_distribution, x_samples, -100, 100, n_iters, 500)
+    plot(mixture_distribution, x_samples, -100, 100, n_iters)
     
 
 
 if __name__ == '__main__':
-    main()
+    
+    parser = argparse.ArgumentParser(description='rw-mh')
+    parser.add_argument('--iters', type=int, default=1000)
+    parser.add_argument('--tau', type=float, default=1.)
+    args = parser.parse_args()
+
+    main(args)
